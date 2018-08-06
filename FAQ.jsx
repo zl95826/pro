e5b0cb111570b509daa592ce80adda1ff1250360
Betty Liu <zl95826@hotmail.com>
2018-08-06 13:06:11 -0700

    new add

---
+++ FAQ.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "../styles/faq.css";
+class FAQ extends Component {
+  render() {
+    return (
+      <div>
+        <h1>FAQ</h1>
+      </div>
+    );
+  }
+}
+
+export default FAQ;
