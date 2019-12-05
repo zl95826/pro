ea730fb7212a3fd6b6652462dff453a216b7739e
zl95826 <zl95826@hotmail.com>
2019-12-04 16:45:42 -0800

    set state

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,22 +3,48 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import ContactData from './ContactData/ContactData';
 import {Route} from 'react-router-dom';
 class Checkout extends Component {
-    state={
-        ingredients:null,/*{
-            salad:1,
-            meat:1,
-            cheese:1,
-            bacon:1
-        }*/
-        totalPrice:0
-    }
+  /**  constructor(props) {
+   *     super(props);
+        const query=new URLSearchParams(this.props.location.search);
+        const ingredients={};
+        let price=0;
+        for (let [key,value] of query) {
+            if(key==='price') {price=+value;}
+           else { ingredients[key]=+value;//convert string to number
+            console.log(+value);}
+        }
+        this.state={
+            ingredients:ingredients,
+            totalPrice:price
+        }
+    }*/
+   //how to do the above code without constructor?
+      initState = () => {
+        const queryInit = new URLSearchParams(
+            this.props.location.search
+        );
+        const ingredientsInit = {};
+        let priceInit = 0;
+        for (let param of queryInit.entries()) {
+            if (param[0] === 'price') {
+                priceInit = param[1];
+            } else {
+                ingredientsInit[param[0]] = +param[1];
+            }
+        }
+        return {
+            ingredients: ingredientsInit,
+            price: priceInit
+        };
+    };
+    state = this.initState(); 
     checkoutCancelHandler=()=>{
         this.props.history.goBack();
     }
     checkoutContinueHandler=()=>{
         this.props.history.replace('checkout/contact-data');
     }
-    componentDidMount() {
+    /*componentDidMount() {
     //I won't use componentDidUpdate or anything like that, because it's not nested in some other
     //page or anything like that. Each time you load this component, it just mount again instead of updating. 
     //If you cancel it or enter another page, you remove/unmount the component, so next time you load into the component
@@ -33,7 +59,7 @@ class Checkout extends Component {
             console.log(+value);}
         }
         this.setState({ingredients:ingredients,totalPrice:price});
-    }
+    }*/
     render() {
         return <div>
                     <CheckoutSummary 
