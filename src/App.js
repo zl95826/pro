5e84691e9419bcda4dca9650ad18e976f61ab465
zl95826 <zl95826@hotmail.com>
2019-12-31 14:11:09 -0800

    auto login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch,withRouter} from 'react-router-dom';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import {connect} from 'react-redux';
+import * as actions from './store/actions/index';
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
   render() {
     return (
       <div>
@@ -23,5 +28,9 @@ class App extends Component {
     );
   }
 }
-
-export default App;
+const mapDispatchToProps=dispatch=>{
+  return {
+    onTryAutoSignup:()=>dispatch(actions.authCheckState())
+  }
+}
+export default withRouter(connect(null,mapDispatchToProps)(App));
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,9 @@ export const authFail=(error)=>{
     }
 }
 export const logout=()=>{
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type:actionTypes.AUTH_LOGOUT
     }
@@ -31,7 +34,7 @@ export const checkAuthTimeout=(expirationTime)=>{
     return dispatch=>{
         setTimeout(()=>{
             dispatch(logout());
-        },expirationTime*1000//turn milliseconds to real seconds
+        },expirationTime*1000//turn seconds to real milliseconds
         );
     }
 }
@@ -44,6 +47,10 @@ export const auth=(email,password,isSignup)=>{
         axios.post(url,authData)
         .then(resp=>{
             console.log(resp);
+            const expirationDate=new Date(new Date().getTime()+resp.data.expiresIn*1000);
+            localStorage.setItem('token',resp.data.idToken);
+            localStorage.setItem('expirationDate',expirationDate);
+            localStorage.setItem('userId',resp.data.localId);
             dispatch(authSuccess(resp.data.idToken,resp.data.localId));
             dispatch(checkAuthTimeout(resp.data.expiresIn));
         })
@@ -55,6 +62,24 @@ export const setAuthRedirectPath=path=>{
     return {
         type:actionTypes.SET_AUTH_REDIRECT_PATH,
         path
+    }
+}
+
+export const authCheckState=()=>{
+    return dispatch=>{
+        const token=localStorage.getItem('token');
+        if(!token) {
+            dispatch(logout());
+        }else {
+            const expirationDate=new Date(localStorage.getItem('expirationDate'));//convert string to date object
+            if(expirationDate<=new Date()) {dispatch(logout());}
+            else {
+                const userId=localStorage.getItem('userId');
+                dispatch(authSuccess(token,userId));
+                dispatch(checkAuthTimeout((expirationDate.getTime()-new Date().getTime())/1000));
+            }
+
+        }
     }
 }
 
